72b2eea633aa98410183f1b3dd4114105eaac7bd
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-12-17 20:30:49 +0300

    add new result presentation

--- app/src/home/home.controller.js
+++ app/src/home/home.controller.js
@@ -11,10 +11,11 @@
  * # HomeCtrl
  * Controller of the hackApp for home page
  */
-hackApp.controller('HomeCtrl', function($scope, Restangular, apiUrl) {
+hackApp.controller('HomeCtrl', function($scope, Restangular, apiUrl, $timeout) {
   $scope.searchUrl = apiUrl + 'search/';
   $scope.query = '';
   $scope.results = [];
+  $scope.framesIndexs = [];
 
   $scope.convertTime = function(secs) {
     return new Date(1000 * secs);
@@ -26,6 +27,26 @@ hackApp.controller('HomeCtrl', function($scope, Restangular, apiUrl) {
     Restangular.oneUrl('search/' + $scope.query).get().then(function(res) {
       console.log(res);
       $scope.results = res;
+      $scope.framesIndexs = new Array($scope.results.length);
+      $scope.framesIndexs.fill(0);
+      $scope.play();
     });
+  };
+
+  $scope.next = function() {
+    for (var i = 0; i < $scope.results.length; i++) {
+      var frames = $scope.results[i].frames;
+      var total = frames.length;
+      if (total > 0) {
+        $scope.framesIndexs[i] = ($scope.framesIndexs[i] === total - 1) ? 0 : $scope.framesIndexs[i] + 1;
+      }
+    }
+  };
+
+  $scope.play = function() {
+    $timeout(function() {
+      $scope.next();
+      $scope.play();
+    }, 3000);
   };
 });
