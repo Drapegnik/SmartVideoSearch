9674736ae13d2c70270da7451e70e6d4e3a4279d
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-12-18 13:48:01 +0300

    add frames list displaying and selection on click

--- app/src/home/home.controller.js
+++ app/src/home/home.controller.js
@@ -15,6 +15,7 @@ hackApp.controller('HomeCtrl', function($scope, Restangular, apiUrl) {
   $scope.searchUrl = apiUrl + 'search/';
   $scope.query = '';
   $scope.results = [];
+  $scope.selectedFrames = [];
 
   $scope.convertTime = function(secs) {
     return new Date(1000 * secs);
@@ -26,6 +27,12 @@ hackApp.controller('HomeCtrl', function($scope, Restangular, apiUrl) {
     Restangular.oneUrl('search/' + $scope.query).get().then(function(res) {
       console.log(res);
       $scope.results = res;
+      $scope.selectedFrames = new Array($scope.results.length);
+      $scope.selectedFrames.fill(0);
     });
+  };
+
+  $scope.selectFrame = function(resInd, frameInd) {
+    $scope.selectedFrames[resInd] = frameInd;
   };
 });
